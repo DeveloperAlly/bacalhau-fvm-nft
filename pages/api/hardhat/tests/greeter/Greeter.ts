aef0948ca9247e29faa7b337e94d7f32d21090d9
Alison Haire <developerally@Alisons-MBP-2.gateway>
2023-01-26 07:53:59 +1100

    type errors

--- pages/api/hardhat/tests/greeter/Greeter.ts
+++ pages/api/hardhat/tests/greeter/Greeter.ts
@@ -1,27 +1,27 @@
-import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
-import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
-import { ethers } from 'hardhat';
+// import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+// import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+// import { ethers } from 'hardhat';
 
-import type { Signers } from '../types';
-import { shouldBehaveLikeGreeter } from './Greeter.behavior';
-import { deployGreeterFixture } from './Greeter.fixture';
+// import type { Signers } from '../types';
+// import { shouldBehaveLikeGreeter } from './Greeter.behavior';
+// import { deployGreeterFixture } from './Greeter.fixture';
 
-describe('Unit tests', function () {
-  before(async function () {
-    this.signers = {} as Signers;
+// describe('Unit tests', function () {
+//   before(async function () {
+//     this.signers = {} as Signers;
 
-    const signers: SignerWithAddress[] = await ethers.getSigners();
-    this.signers.admin = signers[0];
+//     const signers: SignerWithAddress[] = await ethers.getSigners();
+//     this.signers.admin = signers[0];
 
-    this.loadFixture = loadFixture;
-  });
+//     this.loadFixture = loadFixture;
+//   });
 
-  describe('Greeter', function () {
-    beforeEach(async function () {
-      const { greeter } = await this.loadFixture(deployGreeterFixture);
-      this.greeter = greeter;
-    });
+//   describe('Greeter', function () {
+//     beforeEach(async function () {
+//       const { greeter } = await this.loadFixture(deployGreeterFixture);
+//       this.greeter = greeter;
+//     });
 
-    shouldBehaveLikeGreeter();
-  });
-});
+//     shouldBehaveLikeGreeter();
+//   });
+// });
