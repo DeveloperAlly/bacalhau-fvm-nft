b8cc2ffacbc405187421ec7df89204e82746aa74
Alison Haire <developerally@Alisons-MBP-2.gateway>
2023-01-26 08:38:48 +1100

    compiling.....

--- pages/todo.models/User.tsx
+++ pages/todo.models/User.tsx
@@ -1,7 +1,7 @@
 // type Email = new RegExp('^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$')
 // regex type would be a template literal type needing to be defined
 
-interface UserProps {
+export interface UserProps {
   address: string;
   chain: string;
   email?: string;
