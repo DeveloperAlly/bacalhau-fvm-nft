83358562317d34a00a6377767f8b689a3ce2e579
Alison Haire <developerally@Alisons-MBP-2.gateway>
2023-01-31 16:15:13 +1100

    fix next.config error

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'example.com',
-        port: '',
-        pathname: '/.ipfs.nftstorage.link/**',
-      },
-    ],
-  },
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
